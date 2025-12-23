e25d862bbcf728989fae4dc8dacfe41934add0e8
Modit Tyagi <modittyagi163@gmail.com>
2025-12-23 20:44:54 +0000

    why are you redirecting me to (external page) when i am trying to go to

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ require('dotenv').config({ path: './.env.local' });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
