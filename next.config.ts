be73db2c1b550025b0a66b88967fb7b07a8ab295
Modit Tyagi <modittyagi163@gmail.com>
2025-12-23 15:19:26 +0000

    Try fixing this error: `Console Error: Error fetching stays: {}. Error s

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ require('dotenv').config({ path: './.env.local' });
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
